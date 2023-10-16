a7fa60cf85088758b2227dabd257570f279ebd2e
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-10-16 10:11:31 +0000

    first

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from 'react';
+
+const Welcome=({name})=>{
+    return(
+        <div>
+         <h1>{name}</h1>
+         <h2>Welcome to School</h2>
+        </div>
+    )
+}
+export default Welcome;
